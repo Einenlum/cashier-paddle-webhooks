8ec5b2a86d302285f5414a87f53f2a20e91032d2
Einenlum <contact@einenlum.com>
2025-08-25 22:47:05 +0200

    Add routes and config in service provider

--- src/CashierPaddleWebhooksServiceProvider.php
+++ src/CashierPaddleWebhooksServiceProvider.php
@@ -8,6 +8,29 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class CashierPaddleWebhooksServiceProvider extends PackageServiceProvider
 {
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/cashier-paddle-webhooks.php', 'cashier-paddle-webhooks'
+        );
+    }
+
+    public function boot()
+    {
+        $this->configureRoutes();
+        $this->bootPublishing();
+    }
+
+    public function configureRoutes(): void
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
@@ -19,5 +42,19 @@ class CashierPaddleWebhooksServiceProvider extends PackageServiceProvider
             ->name('cashier-paddle-webhooks')
             ->hasConfigFile()
             ->hasCommand(ListenPaddleWebhooksCommand::class);
+    }
+
+    /**
+     * Boot the package's publishable resources.
+     *
+     * @return void
+     */
+    protected function bootPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/cashier-paddle-webhooks.php' => $this->app->configPath('cashier-paddle-webhooks.php'),
+            ], 'cashier-paddle-webhooks-config');
+        }
     }
 }
