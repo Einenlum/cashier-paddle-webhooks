d5727b614c0195ca0fcb5c94b35b76d9793cd97e
Einenlum <contact@einenlum.com>
2025-08-25 22:52:19 +0200

    Ignore Cashier routes in Testbench

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,10 @@
 
 namespace Einenlum\CashierPaddleWebhooks\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Einenlum\CashierPaddleWebhooks\CashierPaddleWebhooksServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Laravel\Paddle\Cashier;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
@@ -12,6 +13,7 @@ class TestCase extends Orchestra
     {
         parent::setUp();
 
+        Cashier::ignoreRoutes();
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'Einenlum\\CashierPaddleWebhooks\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
@@ -27,11 +29,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-         foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/database/migrations') as $migration) {
-            (include $migration->getRealPath())->up();
-         }
-         */
     }
 }
